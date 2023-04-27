46466ca72e7a217d65a38862b8c7505a4291e050
Josh Woods <woo19047@byui.edu>
2023-04-26 18:15:51 -0600

    Finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List <int> numbers = new List<int>();
+        int inputNum = -1;
+
+        while (inputNum != 0)
+        {
+            Console.Write("Please enter some numbers.");
+            string numStr = Console.ReadLine();
+            inputNum = int.Parse(numStr);
+
+            if (inputNum != 0)
+            {
+                numbers.Add(inputNum);
+            }
+        }
+        // Get the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}");
+
+        //  Get the average
+        float avg = ((float)sum)/ numbers.Count(); 
+        Console.WriteLine($"The average is {avg}");
+        int max = 0;
+        foreach (int number in numbers)
+        if (number > max)
+        {
+            max = number;
+        }
+        Console.Write($"The largest number is {max}.");
     }
 }
