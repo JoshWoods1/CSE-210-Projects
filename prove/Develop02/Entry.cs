4ed86703ca806939c30e47a3fe31dcf5caaa626f
Josh Woods <woo19047@byui.edu>
2023-05-03 16:35:04 -0600

    w03 Learning Activity

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,16 @@
+public class Entry{
+
+    public string _prompt;
+
+    public DateTime _date;
+
+    public string _text;
+
+
+
+    public string EnterText()
+    {
+        return _text;
+    }
+
+}
---
+++ prove/Develop02/Prompt.cs
@@ -0,0 +1,19 @@
+public class Prompt 
+{
+    List<string> _prompts = new List<string>();
+
+    public Prompt()
+    {
+
+    }
+
+    public string PickRandomPrompt()
+    {
+        return "this is the prompt";
+    }
+
+    public void AddPrompt()
+    {
+
+    }
+}
